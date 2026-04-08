065d61726430a8ee732f73747194ddc239daef0f
Navya Shukla <navyaa.shukla2812@gmail.com>
2026-04-08 05:48:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1649342616-5bc76",
+  "appId": "1:406746389346:web:aef7cbc3df5e9a3444ea3b",
+  "apiKey": "AIzaSyChS0HmGFgoXXYxL4V31Tw-d15YyWzYHbE",
+  "authDomain": "studio-1649342616-5bc76.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "406746389346"
+};
